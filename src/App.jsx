ba94070d74ef869d08e5e93884afe2b47c5732dd
webQbe <webcubesolutionslk@gmail.com>
2025-05-24 13:15:08 +0530

    Set alternate theme color to button

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ function App() {
   return (
    <div className="App">
     <Button 
-      variant="contained" 
+      variant='contained' 
       /* variant="contained" means:
       The button has a filled background (usually your theme’s primary color)
       It has elevation (drop shadow) to distinguish it from the background. */
+
+      color='secondary' // Set alternate theme color (By default, it’s purple, but it can be customized.)
     >
       Hello World
     </Button>
