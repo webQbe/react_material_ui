2aea52fdc46883142038e94c516e45646f3d46ce
webQbe <webcubesolutionslk@gmail.com>
2025-05-24 13:06:13 +0530

    Added filled background & elevation (drop shadow) to the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
-import Button from '@mui/material/Button';
+import Button from '@mui/material/Button'; // Renders a Material-styled button
 import './App.css'
 
 function App() {
 
   return (
    <div className="App">
-    <Button>
+    <Button 
+      variant="contained" 
+      /* variant="contained" means:
+      The button has a filled background (usually your theme’s primary color)
+      It has elevation (drop shadow) to distinguish it from the background. */
+    >
       Hello World
     </Button>
    </div>
